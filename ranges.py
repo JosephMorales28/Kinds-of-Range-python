dc8c1129a71146039ad85dd58395cc76fd9530b4
Joseph Morales <117571756+JosephMorales28@users.noreply.github.com>
2023-01-20 16:01:25 +0800

    Add files via upload

---
+++ ranges.py
@@ -0,0 +1,12 @@
+numbers =list(range(0,6))
+print(numbers)
+
+for x in range(0,6):
+   print(x)
+
+#even range list from 0 to10
+even=list(range(0,11,2))
+print(even)
+
+for evennumber in range(0,11,2):
+   print(evennumber)
